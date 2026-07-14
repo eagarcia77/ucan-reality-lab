2d372ac193d452fb5e0b58576818aee6011ffb6f
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2026-07-14 13:58:18 -0400

    Register administrator user routes

--- enterprise/backend/app/server.py
+++ enterprise/backend/app/server.py
@@ -2,7 +2,9 @@ from .main import app
 from .ai_routes import router as ai_router
 from .authoring_routes import router as authoring_router
 from .password_routes import router as password_router
+from .admin_routes import router as admin_router
 
 app.include_router(ai_router)
 app.include_router(authoring_router)
 app.include_router(password_router)
+app.include_router(admin_router)
